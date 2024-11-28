d7d523ee616d33f021805587b7a9e60ee699b954
Fawkewe <142065200+Fawkewe@users.noreply.github.com>
2024-11-28 15:41:17 -0500

    Update

--- Assets/Asteroid.cs
+++ Assets/Asteroid.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    public int health;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,16 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health == 0)
+        {
+            Object.Destroy(gameObject);
+            Debug.Log("works");
+        }
+    }
+
+    public void DamageDealt()
+    {
+        int Damage = 1;
+        health = health - Damage;
     }
 }
