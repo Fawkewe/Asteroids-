0616c37f0c38753b27b3257906d648f664006a5d
Fawkewe <142065200+Fawkewe@users.noreply.github.com>
2024-12-08 01:21:10 -0500

    Random.
    Mentinets code btw

--- Assets/Asteroid.cs
+++ Assets/Asteroid.cs
@@ -14,7 +14,9 @@ public class Asteroid : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float RandomX = Random.Range(-1.0f, 1.0f);
+        float RandomY = Random.Range(-1.0f, 1.0f);
+        direction = new Vector2(RandomX, RandomY);
     }
 
     // Update is called once per frame
@@ -23,12 +25,17 @@ public class Asteroid : MonoBehaviour
         float dt = Time.deltaTime;
         Vector3 change = direction * speed * dt;
         transform.position += change;
+
         //movement 
         if (health <= 0 && big)
         {
             Object.Destroy(gameObject);
         }
-
+        if (health <= 0 && medium)
+        {
+            Object.Destroy (gameObject);
+        } 
+        
         if (health <= 0 && small) //The Final function for the Small Asteroid. This should not spilit into anything. 
         {
             Object.Destroy(gameObject);
